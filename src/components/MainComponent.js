86e4e2cfdbb0cf34572962c257f70e0dd3365675
Sean McNair <smcnair@annie-mac.com>
2022-02-07 22:57:48 -0500

    finished Redux Form Feedback

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import About from './AboutComponent'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchCampsites } from '../redux/ActionCreator';
+import { actions } from 'react-redux-form';
 
 
 
@@ -24,7 +25,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-    fetchCampsites: () => (fetchCampsites())
+    fetchCampsites: () => (fetchCampsites()),
+    resetFeedbackForm: () => (actions.reset('feedbackForm'))
 };
 
 class Main extends Component {
@@ -64,7 +66,7 @@ class Main extends Component {
                     <Route exact path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() =><Directory campsites = {this.props.campsites} />}/>
                     <Route exact path='/directory/:campsiteId' component={CampsiteWithId}/>
-                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Route exact path='/about' render={()=><About partners={this.props.partners}/>}/>
                     <Redirect to='/home' />
                 </Switch>
